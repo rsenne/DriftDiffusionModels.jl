3cb8b01628c9221a2fbe92de14af3f141cd8c95c
Ryan Senne <50930199+rsenne@users.noreply.github.com>
2025-04-22 17:21:34 -0400

    Update DDM.jl

--- src/DDM.jl
+++ src/DDM.jl
@@ -18,3 +18,58 @@ function DriftDiffusionModel(
 ) 
     return DriftDiffusionModel(B=B, v=v, a₀=a₀, σ=σ)
 end
+
+"""
+    DDMResult
+
+A tuple of RT and choice. The first element is the RT, the second is the choice.
+"""
+struct DDMResult
+    rt::Float64 # RT
+    choice::Int # Choice (-1 or 1)
+end
+
+function DDMResult(rt::Float64, choice::Int)
+    return DDMResult(rt=rt, choice=choice)
+end
+
+"""
+    Random.rand(model::DriftDiffusionModel)
+
+Generates RT and choices from a drift diffusion model. 
+
+"""
+function Random.rand(model::DriftDiffusionModel)
+
+    @unpack B, v, a₀, σ = model
+
+    # Calculate of crossing the upper bound 
+
+end
+
+"""
+    Distributions.logdensityof(model::DriftDiffusionModel, x::DDMResult)
+
+Calculate the loglikelihood of a DDM result i.e., the tuple of RT and choice.
+"""
+function Distributions.logdensityof(model::DriftDiffusionModel, x::DDMResult)
+
+    @unpack B, v, a₀, σ = model
+    @unpack rt, choice = x
+
+    # Calculate the loglikelihood of the DDM result
+end
+
+"""
+    StatsAPI.fit!(model::DriftDiffusionModel, x::Vector{DDMResult}, w::Vector{Float64}=ones(length(x)))
+
+Perform parameter estimation of a drift diffusion model using MLE given a vector of DDM observtions. Takes an optional weights vector to support for use in an HMM.
+"""
+function StatsAPI.fit!(model::DriftDiffusionModel, x::Vector{DDMResult}, w::Vector{Float64}=ones(length(x)))
+    
+        @unpack B, v, a₀, σ = model
+
+        
+    end
+
+    
